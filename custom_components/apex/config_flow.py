4a03883a0d383cc81c44630d8a7dcbae21131f39
bugbountyguy <88092414+bugbountyguy@users.noreply.github.com>
2026-07-01 21:01:39 -0500

    Fix 500 in OptionsFlow

--- custom_components/apex/config_flow.py
+++ custom_components/apex/config_flow.py
@@ -83,8 +83,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
 class OptionsFlow(config_entries.OptionsFlow):
     def __init__(self, config_entry: config_entries.ConfigEntry):
-        """Initialize options flow."""
-        self.config_entry = config_entry
+        pass
 
     async def async_step_init(self, user_input=None):
         if user_input is not None:
